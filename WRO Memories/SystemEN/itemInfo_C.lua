3809c3b0ce0f63a2e149e45c42cb0a51e820a116
Nihad <nihadm89@hotmail.com>
2025-12-30 12:35:12 +0100

    Main System Folder

---
+++ WRO Memories/SystemEN/itemInfo_C.lua
@@ -0,0 +1,55 @@
+--[[ Template
+	[ID] = {
+		unidentifiedDisplayName = "Unknown Item",
+		unidentifiedResourceName = "",
+		unidentifiedDescriptionName = { "" },
+		identifiedDisplayName = "Item",
+		identifiedResourceName = "",
+		identifiedDescriptionName = {
+			"Line 1",
+			"Line 2"
+		},
+		slotCount = 0,
+		ClassNum = 0,
+		costume = false
+	},
+]]
+
+-- Table for Custom Items
+tbl_custom = {
+	[8965] = {
+		unidentifiedDisplayName = "Blue Skull",
+		unidentifiedResourceName = "blueskull",
+		unidentifiedDescriptionName = {
+		},
+		identifiedDisplayName = "Blue Skull",
+		identifiedResourceName = "blueskull",
+		identifiedDescriptionName = {
+			"A skull of a killed Guillaume Warrior.",
+			"Weight :^777777 0^000000",
+			"^FFA500Extended Battleground^000000"
+		},
+		slotCount = 0,
+		ClassNum = 0
+	},
+	[8966] = {
+		unidentifiedDisplayName = "Red Skull",
+		unidentifiedResourceName = "redskull",
+		unidentifiedDescriptionName = {
+		},
+		identifiedDisplayName = "Red Skull",
+		identifiedResourceName = "redskull",
+		identifiedDescriptionName = {
+			"A skull of a killed Croix Warrior..",
+			"Weight :^777777 0^000000",
+			"^FFA500Extended Battleground^000000"
+		},
+		slotCount = 0,
+		ClassNum = 0
+	}
+}
+
+-- Table for Official Overrides
+tbl_override = {
+	
+}
